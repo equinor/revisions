86084da424380a4d018140474f40ff392c0badcd
Johannes Telle <johannes.telle@bouvet.no>
2024-03-25 15:20:30 +0100

    Unit tests should now fail

--- src/DtoTransformer/Test/Program.cs
+++ src/DtoTransformer/Test/Program.cs
@@ -1,7 +1,5 @@
 ﻿using VDS.RDF.Writing;
 using Review;
-using System.Runtime.CompilerServices;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 var reviewDto = new ReviewDTO
@@ -19,7 +17,7 @@ var reviewDto = new ReviewDTO
 var commentDto = new CommentDto
 {
     CommentUri = new Uri($"https://rdf.equinor.com/data/review/comment/{Guid.NewGuid()}"),
-    CommentText = "A comment kjfdfbn hehje  hefhekfje  kjdhfdkj khdjkhdjk k hj h kfhdkjskjdhd kdjfhdjfh jkdhfdj jd kjddj  jjd .",
+    CommentText = "A comment",
     IssuedBy = "Johannes",
     GeneratedAtTime = DateOnly.FromDateTime(DateTime.Now),
     AboutData = new List<Uri>()
@@ -53,10 +51,8 @@ var anotherCommentDto = new CommentDto
     }
 };
 
-
 reviewDto.HasComments.Add(commentDto);
 reviewDto.HasComments.Add(anotherCommentDto);
-
 
 Console.Write("First DTO");
 printInfo(reviewDto);
--- src/DtoTransformer/Tests/Tests.cs
+++ src/DtoTransformer/Tests/Tests.cs
@@ -10,6 +10,7 @@ using static Review.Namespaces;
 using VDS.RDF.Shacl.Validation;
 using Xunit.Abstractions;
 using FluentAssertions;
+using ClosedXML.Excel;
 
 
 namespace Review.Tests
@@ -43,8 +44,13 @@ namespace Review.Tests
             {
                 var expectedComment = reviewDto.HasComments[i];
                 var actualComment = reviewDtoAfterTransformation.HasComments[i];
-                Assert.Equal(expectedComment.CommentId, actualComment.CommentId);
-                Assert.Equal(expectedComment.CommentText, actualComment.CommentText);
+
+                Guid parsedGuid;
+                bool isValidGuid = Guid.TryParse(actualComment.CommentId.ToString(), out parsedGuid);
+                Assert.True(isValidGuid, "The CommentId is not a valid GUID.");
+
+                Assert.False(string.IsNullOrEmpty(actualComment.CommentText), "The CommentText should not be null or empty.");
+
             }
         }
         [Fact]
@@ -79,40 +85,96 @@ namespace Review.Tests
              report.Conforms.Should().BeTrue();
         }
 
+
+        [Fact]
+        public void ValidateThatExcelIsCorrect()
+        {
+            // Arrange
+            var reviewDto = CreateReviewDto();
+            var expectedFilePath = "C:/Users/johannes.telle//source/repos/revisions/src/DtoTransformer/Tests/TestData/CorrectOutput.xlsx";
+            var actualFilePath = "output.xlsx";
+
+            // Act
+            var graph = RdfGenerator.GenerateRdf(reviewDto);
+            var reviewDtoAfterTransformation = DtoGenerator.GenerateDto(graph);
+            ExcelGenerator.CreateExcelAt(reviewDtoAfterTransformation, actualFilePath);
+
+            // Assert
+            using (var expectedWorkbook = new XLWorkbook(expectedFilePath))
+            using (var actualWorkbook = new XLWorkbook(actualFilePath))
+            {
+                foreach (var expectedSheet in expectedWorkbook.Worksheets)
+                {
+                    var actualSheet = actualWorkbook.Worksheet(expectedSheet.Name);
+                    foreach (var expectedCell in expectedSheet.CellsUsed())
+                    {
+                        if (expectedCell.GetText().Contains("comment:"))
+                        {
+                            //pass
+                        }
+                        else
+                        {
+                            var actualCell = actualSheet.Cell(expectedCell.Address);
+                            Assert.Equal(expectedCell.Value, actualCell.Value);
+                        }
+                    }
+                }
+            }
+        }
+
         public ReviewDTO CreateReviewDto() {
             var reviewDto = new ReviewDTO
             {
                 ReviewId = "https://example.com/doc/reply-A123-BC-D-EF-00001.F01",
                 AboutRevision = new Uri("https://example.com/data/A123-BC-D-EF-00001.F01"),
                 IssuedBy = "Turi Skogen",
-                GeneratedAtTime = DateOnly.FromDateTime(DateTime.UtcNow),
+                GeneratedAtTime = DateOnly.FromDateTime(DateTime.Now),
                 ReviewStatus = "https://rdf.equinor.com/ontology/review/Code1",
                 Label = "Reply to revision F01",
                 HasComments = new List<CommentDto>()
             };
+
 
             var commentDto = new CommentDto
             {
                 CommentUri = new Uri($"https://rdf.equinor.com/data/review/comment/{Guid.NewGuid()}"),
                 CommentText = "A comment",
                 IssuedBy = "Johannes",
-                GeneratedAtTime = DateOnly.FromDateTime(DateTime.UtcNow),
-                AboutData = new List<Uri>
-                {
-                    new Uri("https://example.com/doc/A123-BC-D-EF-00001.F01row1"),
-                    new Uri("https://example.com/doc/A123-BC-D-EF-00001.F01row3"),
-                    new Uri("https://example.com/doc/A123-BC-D-EF-00001.F01row10")
-                },
-                AboutObject = new List<(Uri property, string value)>
-                {
-                    (new Uri("https://rdf.equinor.com/ontology/mel/v1#tagNumber"), "the tag number"),
-                    (new Uri("https://rdf.equinor.com/ontology/mel/v1#weightHandlingCode"), "The handling code"),
-                    (new Uri("https://rdf.equinor.com/ontology/mel/v1#importantField"), "The important field")
-                }
+                GeneratedAtTime = DateOnly.FromDateTime(DateTime.Now),
+                AboutData = new List<Uri>()
+    {
+        new Uri("https://example.com/doc/A123-BC-D-EF-00001.F01row1"),
+        new Uri("https://example.com/doc/A123-BC-D-EF-00001.F01row3"),
+        new Uri("https://example.com/doc/A123-BC-D-EF-00001.F01row10")
+    },
+                AboutObject = new List<(Uri property, string value)>()
+    {
+        (new Uri("https://rdf.equinor.com/ontology/mel/v1#tagNumber"), "the tag number"),
+        (new Uri("https://rdf.equinor.com/ontology/mel/v1#weightHandlingCode"), "The handling code"),
+        (new Uri("https://rdf.equinor.com/ontology/mel/v1#importantField"), "The important field")
+    }
             };
 
+            var anotherCommentDto = new CommentDto
+            {
+                CommentUri = new Uri($"https://rdf.equinor.com/data/review/comment/{Guid.NewGuid()}"),
+                CommentText = "Another comment",
+                IssuedBy = "John Doe",
+                GeneratedAtTime = DateOnly.FromDateTime(DateTime.Now),
+                AboutData = new List<Uri>()
+    {
+        new Uri("https://example.com/doc/AnotherDocument.Row1")
+    },
+                AboutObject = new List<(Uri property, string value)>()
+    {
+        (new Uri("https://rdf.equinor.com/ontology/mel/v1#tagNumber"), "the tag number"),
+        (new Uri("https://rdf.equinor.com/ontology/mel/v1#weightHandlingCode"), "The handling code")
+    }
+            };
 
             reviewDto.HasComments.Add(commentDto);
+            reviewDto.HasComments.Add(anotherCommentDto);
+
             return reviewDto;
         }
 
