25ec2f700da97e7e84fa4c24955adbf103355731
Henriette Lien Rebnor <122604807+henriettelienrebnor@users.noreply.github.com>
2024-08-12 11:51:59 +0200

    Update Dto and Rdf Generator (#43)
    * Test pass
    
    * fix failing test

--- src/DtoTransformer/DtoTransformer/TR.cs
+++ src/DtoTransformer/DtoTransformer/TR.cs
@@ -10,10 +10,17 @@ public static class TRExtensions
 {
     public const string MelReportingTemplateUri = "https://rdf.equinor.com/ontology/technical-requirement/v1#MelReportingTemplate";
 
-    public static string ToUri(this TR type) =>
+    public static Uri ToUri(this TR type) =>
         type switch
         {
-            TR.MelReportingTemplate => MelReportingTemplateUri,
+            TR.MelReportingTemplate => new Uri(MelReportingTemplateUri),
             _ => throw new ArgumentOutOfRangeException(nameof(type))
+        };
+
+    public static TR StringUriToTR(string stringUri) =>
+        stringUri switch
+        {
+            MelReportingTemplateUri => TR.MelReportingTemplate,
+            _ => throw new ArgumentOutOfRangeException(stringUri)
         };
 }
